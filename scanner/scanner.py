613edf6992ca8d2f783b557373e64040414c8568
alimohammadiamirhossein <45646480+alimohammadiamirhossein@users.noreply.github.com>
2021-03-28 00:59:32 +0430

    Initial commit

---
+++ scanner/scanner.py
@@ -0,0 +1,14 @@
+from scanner.file_reader import FileReader
+from scanner.regex import Regex
+
+fr = FileReader(path="input.txt")
+regex_ = Regex()
+current_state = regex_.state_zero
+
+while not fr.is_last_line:
+    if fr.current_char == len(fr.backup_line) - 1:
+        fr.load_backup_line()
+    x = fr.forward_read()
+    current_state = current_state.get_next_state(x)
+
+
