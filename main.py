c159b9067f8d8757c9914aade98ae91d57116ed3
amir mahdi hosseinabadi <hosseinabadi26@gmail.com>
2021-04-03 21:56:32 +0430

    error handling

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ fr = FileReader(path="scanner//input.txt")
 regex_ = Regex()
 current_state = regex_.state_zero
 
+
 while not fr.is_last_line:
     if fr.current_char == len(fr.backup_line) - 1:
         fr.load_backup_line()
