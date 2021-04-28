53b3497164dbaf4be7f5e03c3e15a46de29ac30c
amir mahdi hosseinabadi <hosseinabadi26@gmail.com>
2021-04-28 13:50:40 +0430

    first and follow

--- parsr/txtReader.py
+++ parsr/txtReader.py
@@ -1,8 +1,9 @@
+# follow data
+
 file = open("firstdata.txt", "r")
 first = open("first.txt", "w")
 terminals = ['ID', ';', '[', 'NUM', ']', '(', ')', 'int', 'void', ',','{', '}', 'break', 'if', 'else', 'while', 'return',
              'for', '=', '<', '==', '+', '-', '*', 'epsilon']
-# print(len(terminals))
 for i in range(51):
     line = file.readline()
     array = line.split()
@@ -10,9 +11,26 @@ for i in range(51):
     # print(len(array))
     for j in range(1, len(array)):
         if array[j] == "+":
-            print(j , array[0])
             first.write(terminals[j-1]+" ")
     first.write("\n")
 
 first.close()
+file.close()
+
+# follow data
+file = open("followdata", "r")
+follow = open("follow.txt", "w")
+terminals = ['ID', ';', '[', 'NUM', ']', '(', ')', 'int', 'void', ',','{', '}', 'break', 'if', 'else', 'while', 'return',
+             'for', '=', '<', '==', '+', '-', '*', 'epsilon']
+for i in range(51):
+    line = file.readline()
+    array = line.split()
+    follow.write(array[0] + " -> ")
+    # print(len(array))
+    for j in range(1, len(array)):
+        if array[j] == "+":
+            follow.write(terminals[j-1]+" ")
+    follow.write("\n")
+
+follow.close()
 file.close()
