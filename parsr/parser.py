6fdd6622a468993006c8748ab5c94c154023cf2e
alimohammadiamirhossein <alimohammadiamirhossein@gmail.com>
2021-04-30 01:17:14 +0430

    parse tree show

--- parsr/parser.py
+++ parsr/parser.py
@@ -87,25 +87,30 @@ class Parser:
         self.lookahead = scanner.get_token()
         file = open("report/parse_tree.txt", "w+", encoding='utf-8')
         errors = open("report/errors.txt", "w+", encoding='utf-8')
-        self.Procedure("Program",  1, scanner, 0, file, errors)
+        node1 = Node("Program")
+        self.Procedure("Program",  1, scanner, 0, file, errors, node1)
+        result = ""
+        for pre, fill, node in RenderTree(node1):
+            if node.name == 'ε':
+                result += f"{pre}epsilon\n"
+            else:
+                result += f"{pre}{node.name}\n"
+        print(result)
+        file.write(result)
         file.close()
         errors.close()
 
     def Procedure(self, nonTerminal,  lineNumber, scannar1, tabs, file,
-                  errors):  # we have to have line number
-        for i in range(tabs):
-            file.write("\t".rstrip('\n'))
-        file.write("%s \n" % (nonTerminal))
+                  errors, parent):  # we have to have line number
         i = 2
         if self.lookahead[1] == "NUM" or self.lookahead[1] == "ID":
             i = 1
+        node1 = Node(nonTerminal, parent=parent)
         if self.parse_table[nonTerminal][self.lookahead[i]][0][0] != "empty":  # checks if it is not empty
             if self.parse_table[nonTerminal][self.lookahead[i]][0][0] == "ε":
+                node1 = Node("epsilon", parent=parent)
                 print(nonTerminal)
                 print(self.parse_table[nonTerminal])
-                for i in range(tabs + 1):
-                    file.write("\t".rstrip('\n'))
-                file.write("epsilon \n")
                 # return
             elif self.parse_table[nonTerminal][self.lookahead[i]][0][0] == "sync":
                 # errors.write("missing %s on line %s \n", (nonTerminalObject.first[0], lineNumber))
@@ -115,22 +120,23 @@ class Parser:
                     temp = x[0]
                     temp1 = x[1]
                     if temp1 == "Non-Terminal":
-                        self.Procedure(temp, lineNumber, scannar1, tabs + 1, file, errors)
+                        self.Procedure(temp, lineNumber, scannar1, tabs + 1, file, errors, node1)
                     else:
-                        self.Match(temp, lineNumber, scannar1, tabs + 1, file, errors)
+                        self.Match(temp, lineNumber, scannar1, tabs + 1, file, errors, node1)
         else:  # if it is empty
             errors.write("illegal %s on line %s \n" % (self.lookahead, lineNumber))
             self.lookahead = scannar1.get_token()
-            self.Procedure(nonTerminal, lineNumber, scannar1, tabs + 1, file, errors)
+            self.Procedure(nonTerminal, lineNumber, scannar1, tabs + 1, file, errors, node1)
 
-    def Match(self, terminal, lineNumber, scanner, tabs, file, errors):
+    def Match(self, terminal, lineNumber, scanner, tabs, file, errors, parent):
         i = 2
         if self.lookahead[1] == "NUM" or self.lookahead[1] == "ID":
             i = 1
+        node1 = Node(terminal, parent=parent)
         if self.lookahead[i] == terminal:
             for i in range(tabs):
                 file.write("\t".rstrip('\n'))
-            file.write("%s \n" % (terminal))
+            # file.write("%s \n" % (terminal))
             self.lookahead = scanner.get_token()
         else:
             errors.write("missing %s on line %s \n" % (terminal, lineNumber))
