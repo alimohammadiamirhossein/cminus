70b4600bbadb6497f1f1d670996df0a6858382f8
alimohammadiamirhossein <alimohammadiamirhossein@gmail.com>
2021-05-29 19:31:28 +0430

    add function to codegen.py

--- codegen/codegen.py
+++ codegen/codegen.py
@@ -1,6 +1,7 @@
 class CodeGen:
     def __init__(self , symbol_table):
         self.semantic_stack = []
+        self.program_block = []
         self.symbol_table = symbol_table
         self.tempVarIndex = 500
         self.top = 0
@@ -27,3 +28,36 @@ class CodeGen:
         add = self.getAddress(token)
         self.semantic_stack.append(add)
         self.top += 1
+
+    def pnum(self, token):
+        self.semantic_stack.append(f"#{token}")
+
+
+    def parr(self, token=None):
+        len1 = self.semantic_stack.pop()
+        tmp_address = self.getTemp()
+        array_start_address = self.semantic_stack.pop()
+        self.program_block.append(f"MULT, #4, {len1}, {tmp_address}")
+        self.program_block.append(f"ADD, {array_start_address}, {tmp_address}, {tmp_address}")
+        self.semantic_stack.append(f"{tmp_address}")
+
+
+    #def declare_id(self):
+        #ssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+
+    def declare_arr(self, token=None):
+        len1 = self.semantic_stack.pop()
+        len1 = int(len1[1:])
+        len1 -= 1
+        self.top += 4 * len1
+
+    def assign(self, token=None):
+        value = self.semantic_stack.pop()
+        assign_par = self.semantic_stack.pop()
+        self.program_block.append(f"(ASSIGN, {value}, {assign_par}, )")
+        self.semantic_stack.append(assign_par)
+
+    def op_push(self, token):
+        self.semantic_stack.append(self.operands[token.lexeme])
+
+    #def op_exec():
